a8699db2bddef520aeefe653a28064777f530ce1
Okash Khawaja <okash.khawaja@gmail.com>
2023-06-17 08:54:40 +0100

    programming drill 1.2.1
    This adds subtraction, division, modulus and conjugate.

--- src/ch1_complex_numbers.rs
+++ src/ch1_complex_numbers.rs
@@ -13,6 +13,14 @@ impl Complex {
             imaginary: i
         }
     }
+
+    pub fn modulus(&self) -> f64 {
+        (self.real.powi(2) + self.imaginary.powi(2)).sqrt()
+    }
+
+    pub fn conjugate(&self) -> Complex {
+        Complex::new(self.real, -1.0 * self.imaginary)
+    }
 }
 
 impl ops::Add<&Complex> for &Complex {
@@ -22,10 +30,18 @@ impl ops::Add<&Complex> for &Complex {
         let rsum = self.real + rhs.real;
         let isum = self.imaginary + rhs.imaginary;
 
-        Complex {
-            real : rsum,
-            imaginary : isum,
-        }
+        Complex::new(rsum, isum)
+    }
+}
+
+impl ops::Sub<&Complex> for &Complex {
+    type Output = Complex;
+
+    fn sub(self, rhs: &Complex) -> Complex {
+        let rdiff = self.real - rhs.real;
+        let idiff = self.imaginary - rhs.imaginary;
+
+        Complex::new(rdiff, idiff)
     }
 }
 
@@ -36,10 +52,19 @@ impl ops::Mul<&Complex> for &Complex {
         let r = (self.real * rhs.real) + (self.imaginary * rhs.imaginary * -1.0);
         let i = (self.real * rhs.imaginary) + (self.imaginary * rhs.real);
 
-        Complex {
-            real: r,
-            imaginary: i
-        }
+        Complex::new(r, i)
+    }
+}
+
+impl ops::Div<&Complex> for &Complex {
+    type Output = Complex;
+
+    fn div(self, rhs: &Complex) -> Complex {
+        let denominator = rhs.real.powi(2) + rhs.imaginary.powi(2);
+        let rquotient = ((self.real * rhs.real) + (self.imaginary * rhs.imaginary)) / denominator;
+        let iquotient = ((rhs.real * self.imaginary) - (self.real * rhs.imaginary)) / denominator;
+
+        Complex::new(rquotient, iquotient)
     }
 }
 
@@ -59,4 +84,42 @@ fn test_add_and_multiply() {
     assert_eq!(csum.imaginary, 3.0);
     assert_eq!(cprod.real, 13.0);
     assert_eq!(cprod.imaginary, -1.0);
+}
+
+#[test]
+fn test_subtract() {
+    let c1 = Complex::new(1.0, -2.0);
+    let c2 = Complex::new(3.0, 5.0);
+
+    let cdiff = &c1 - &c2;
+
+    assert_eq!(cdiff.real, -2.0);
+    assert_eq!(cdiff.imaginary, -7.0);
+}
+
+#[test]
+fn test_divide() {
+    let c1 = Complex::new(-2.0, 1.0);
+    let c2 = Complex::new(1.0, 2.0);
+
+    let cdiff = &c1 / &c2;
+
+    assert_eq!(cdiff.real, 0.0);
+    assert_eq!(cdiff.imaginary, 1.0);
+}
+
+#[test]
+fn test_modulus() {
+    let c = Complex::new(3.0, 4.0);
+
+    assert_eq!(c.modulus(), 5.0);
+}
+
+#[test]
+fn test_conjugate() {
+    let c = Complex::new(3.0, 4.0);
+    let conj = c.conjugate();
+
+    assert_eq!(conj.real, c.real);
+    assert_eq!(conj.imaginary, -1.0*c.imaginary);
 }
